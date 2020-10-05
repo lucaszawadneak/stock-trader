8b5c8b0c95e7a490c378670a8cc93a1924c5b8bc
Lucas <lucaszawadneak@gmail.com>
2020-10-05 09:28:24 -0300

    bug fixes v2

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -4,13 +4,19 @@
       <h1>Trade and consult your stocks</h1>
       <b-alert show variant="primary">You can load or save your data!</b-alert>
       <b-alert show variant="secondary">Press end day to start a new day!</b-alert>
-      <span>Your balance: R$100,00</span>
+      <span>Your balance: {{ balance | money }}</span>
     </div>
   </div>
 </template>
 
 <script>
-export default {};
+export default {
+  computed: {
+    balance() {
+      return this.$store.state.balance;
+    },
+  },
+};
 </script>
 
 <style scoped></style>
